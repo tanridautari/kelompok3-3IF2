a49bd80f7961a97aa2cda3fe3562e1c7721d191b
tanridautari <tanridautari1@gmail.com>
2019-07-15 11:19:50 +0700

    update

--- controllers/books.js
+++ controllers/books.js
@@ -21,3 +21,28 @@ module.exports.postBooks = (req, res) => {
 	
 }	
 
+module.exports.putBooks = (req, res) => {
+let values = {
+  judul_buku: req.body.judul_buku,
+  penulis: req.body.penulis,
+  ISBN: req.body.ISBN,
+  jumlah_halaman: req.body.jumlah_halaman,
+  tgl_terbit: req.body.tgl_terbit,
+  harga: req.body.harga,
+  penerbit: req.body.penerbit
+} 
+let conditions = {
+ where:{
+  id: req.params.id
+ }
+}
+
+  Books
+  .update(values, conditions)
+  .then((books) => {
+   res.json(books);
+  }).catch((error) => {
+  console.log(error);
+ })
+}
+
--- routes/books.js
+++ routes/books.js
@@ -6,5 +6,6 @@ const booksController = require('../controllers/books');
 
 router.get('/', booksController.getIndexBooks);
 router.post('/', booksController.postBooks);
+router.put('/:id', booksController.putBooks);
 
 module.exports = router;
