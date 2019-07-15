3292ad31f75c89a16175ac28942720758cc2f750
tanridautari <tanridautari1@gmail.com>
2019-07-15 14:41:58 +0700

    hak akses admin

--- controllers/books.js
+++ controllers/books.js
@@ -6,62 +6,87 @@ module.exports.getIndexBooks = (req, res) => {
 		if(error) {
 			res.sendStatus(403);
 		} else { 
-		res.json({
-			message: 'OK',
-			authData: authData
-		})
+			res.json({
+				message: 'OK',
+				authData: authData
+			})
 		}
 	})
 }
 
 module.exports.postBooks = (req, res) => {
-	Books.create({
-		id: req.body.id,
-		judul_buku: req.body.judul_buku,
-		penulis: req.body.penulis,
-		ISBN: req.body.ISBN,
-		jumlah_halaman: req.body.jumlah_halaman,
-		tgl_terbit: req.body.tgl_terbit,
-		harga: req.body.harga,
-		penerbit: req.body.penerbit
-	}).then((books) => {
-		res.json(books);
-	}).catch((error) => {
-		console.log(error);
+	jwt.verify(req.token, process.env.SECRETKEY, (error, authData) => {
+		if(error) {
+			res.sendStatus(403);
+		} else {
+			if (authData['roles']=="admin") {
+				Books.create({
+					id: req.body.id,
+					judul_buku: req.body.judul_buku,
+					penulis: req.body.penulis,
+					ISBN: req.body.ISBN,
+					jumlah_halaman: req.body.jumlah_halaman,
+					tgl_terbit: req.body.tgl_terbit,
+					harga: req.body.harga,
+					penerbit: req.body.penerbit
+				}).then((books) => {
+					res.json(books);
+				}).catch((error) => {
+					console.log(error);
+				})
+			} 
+			
+		}
 	})
-	
 }	
 
 module.exports.putBooks = (req, res) => {
-let values = {
-  judul_buku: req.body.judul_buku,
-  penulis: req.body.penulis,
-  ISBN: req.body.ISBN,
-  jumlah_halaman: req.body.jumlah_halaman,
-  tgl_terbit: req.body.tgl_terbit,
-  harga: req.body.harga,
-  penerbit: req.body.penerbit
-} 
-let conditions = {
- where:{
-  id: req.params.id
- }
-}
+	jwt.verify(req.token, process.env.SECRETKEY, (error, authData) => {
+		if(error) {
+			res.sendStatus(403);
+		} else {
+			if (authData['roles']=="admin") {
+				let values = {
+					judul_buku: req.body.judul_buku,
+					penulis: req.body.penulis,
+					ISBN: req.body.ISBN,
+					jumlah_halaman: req.body.jumlah_halaman,
+					tgl_terbit: req.body.tgl_terbit,
+					harga: req.body.harga,
+					penerbit: req.body.penerbit
+				} 
+				let conditions = {
+					where:{
+						id: req.params.id
+					}
+				}
 
-  Books
-  .update(values, conditions)
-  .then((books) => {
-   res.json(books);
-  }).catch((error) => {
-  console.log(error);
- })
-}
+				Books
+				.update(values, conditions)
+				.then((books) => {
+					res.json(books);
+				}).catch((error) => {
+					console.log(error);
+				})
+			} 
+			
+		}
+	})
+}	
 
 module.exports.deleteBooks = (req, res) => {
-	Books.destroy({
-		where: {
-			id: req.params.id
+	jwt.verify(req.token, process.env.SECRETKEY, (error, authData) => {
+		if(error) {
+			res.sendStatus(403);
+		} else {
+			if (authData['roles']=="admin") {
+				Books.destroy({
+					where: {
+						id: req.params.id
+					}
+				}).then((result) => res.json(result))
+			} 
 		}
-	}).then((result) => res.json(result))
+	})
 }
 
